d90d7eda2f1f91cfc805a0caf087791fb01f00a7
Nathan-Simonnet <nathan.simonnet@gmail.com>
2023-11-17 12:26:53 +0100

    js question display

--- questions/script.js
+++ questions/script.js
@@ -1,6 +1,36 @@
-const category = "sport"
-const categoryContainer = document.querySelector('div.categoryContainer');
+let questionsIndex = 0;
 
-console.log(categoryContainer)
+let questions = [{
+    difficutly: "hard",
+    category: "sport",
+    title: "Quel sport est également appelé le noble art?",
+    answer: ["Le polo", "La boxe anglaise", "le tennis", "réponse D"]
+}, {
+    difficutly: "hard",
+    category: "nature",
+    title: "Le pyton sait-il vraiment programmer?",
+    answer: ["oui", "non", "peut-être", "haha trop marrant lol xd"]
+}]
 
-categoryContainer.classList.add(category)
+const questionDisplayer = function () {
+
+
+    const categoryContainer = document.querySelector('div.categoryContainer');
+
+    document.querySelector('span.difficultySpan').textContent = questions[questionsIndex].difficutly == "hard" ? "Difficile" : questions[questionsIndex].difficutly == "medium" ? "Moyen" : "Facile";
+    document.querySelector('span.questionsSpan').textContent = toString(questionsIndex);
+    categoryContainer.classList.add(questions[questionsIndex].category)
+
+    document.querySelector('h3').textContent = questions[questionsIndex].title;
+
+    let indexQuestionDisplay = 1;
+    for (let i = 0; i < questions[questionsIndex].answer.length; i++) {
+        document.querySelector('.answer' + indexQuestionDisplay).textContent = questions[questionsIndex].answer[indexQuestionDisplay - 1]
+        indexQuestionDisplay++;
+    }
+}
+
+questionDisplayer();
+
+
+
